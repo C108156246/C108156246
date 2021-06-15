65b7a6b5b79da8b2762b6533eb4514d5617f2389
C108156246 <C108156246@nkust.edu.tw>
2021-06-16 00:53:50 +0800

    Init 0401

---
+++ 0401/App2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+(
+    <div>
+        <Header/>
+        <Content/>
+    </div>  
+)
+
+(
+    <div>
+        <h1>Header</h1>
+    </div>
+)
+
+(
+    <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
+    </div>
+)
+
+export default App;
